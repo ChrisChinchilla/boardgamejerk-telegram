c251a8b5f998d77e9db36f5415abb1742dde721f
Chris Ward <chriswhward@gmail.com>
2018-06-24 12:53:58 +0200

    First automated build

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var tracery = require('tracery-grammar');
-var you_are = require('./you_are.json');
-var doing = require('./doing.json');
-var with_what = require('./with_what.json');
-var in_place = require('./in.json');
+let tracery = require('tracery-grammar');
+let Twit = require('twit');
+require('dotenv').config();
 
-var grammar = tracery.createGrammar({
+let you_are = require('./you_are.json');
+let doing = require('./doing.json');
+let with_what = require('./with_what.json');
+let in_place = require('./in.json');
+
+const grammar = tracery.createGrammar({
     "you_are": you_are,
     "doing": doing,
     "with_what": with_what,
@@ -15,3 +18,17 @@ var grammar = tracery.createGrammar({
 grammar.addModifiers(tracery.baseEngModifiers);
 
 console.log(grammar.flatten('#origin#'));
+const tweet = grammar.flatten('#origin#');
+
+const T = new Twit(
+    {
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token: process.env.TWITTER_ACCESS_TOKEN,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    }
+);
+
+T.post('statuses/update', {status: tweet + " #boardgames"}, (err, data, response) => {
+    console.log(data)
+});
