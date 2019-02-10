43aaf3e17c5acfba9c4df066daa6ef6497961ca0
Chris Ward <chriswhward@gmail.com>
2019-02-10 12:26:50 +0000

    Refactor to use NPM package

--- dialogflow.js
+++ dialogflow.js
@@ -1,19 +1,18 @@
-'use strict';
+"use strict";
 
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  request = require("request"),
+  app = express().use(bodyParser.json()); // creates express http server
 
-const
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    request = require('request'),
-    app = express().use(bodyParser.json()); // creates express http server
-
-var tweetLexicon = require('./createlexicon.js');
+// TODO: Refactor correctly
+var tweetLexicon = require("./node_modules/bgjg/index.js");
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 5000, () => console.log("webhook is listening"));
 
-app.post('/webhook', (req, res) => {
-    var chat = tweetLexicon.randomString();
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ "fulfillmentText": chat }));
+app.post("/webhook", (req, res) => {
+  var chat = tweetLexicon.generateLexicon();
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ fulfillmentText: chat }));
 });
--- twitter.js
+++ twitter.js
@@ -1,21 +1,25 @@
-let Twit = require('twit');
-require('dotenv').config();
+// TODO: Is this still needed? Or via Dialogflow?
+let Twit = require("twit");
+require("dotenv").config();
 
-var tweetLexicon = require('./createlexicon.js');
+// TODO: Refactor correctly
+var tweetLexicon = require("./node_modules/bgjg/index.js");
 
-var tweetText = tweetLexicon.randomString();
+var tweetText = tweetLexicon.generateLexicon();
 
-const T = new Twit(
-    {
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token: process.env.TWITTER_ACCESS_TOKEN,
-        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    }
-);
+const T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 console.log(tweetText);
 
-T.post('statuses/update', {status: tweetText + " #boardgames"}, (err, data, response) => {
-    console.log(data)
-});
+T.post(
+  "statuses/update",
+  { status: tweetText + " #boardgames" },
+  (err, data, response) => {
+    console.log(data);
+  }
+);
